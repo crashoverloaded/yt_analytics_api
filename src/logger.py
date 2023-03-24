dc6e54f1f62651de888c0e9883f8d109f944a38e
crashoverloaded <2017pgicspriyank033@poornima.org>
2023-03-24 20:40:10 +0530

    Logging part added

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+import logging
+import logging as lg
+import os
+from datetime import datetime
+
+# creating a log file name based on date and time
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Path of log file
+logs_path = os.path.join(os.getcwd(),"logs")
+
+# Exist_ok = True is set to append the file and folder as per need  hence no need to create the structure again and again
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+lg.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=lg.INFO
+)
